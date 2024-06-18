6a2fb9f938630cc9e08afa537e4910a247a6dc98
unknown <eliseo101801@gmail.com>
2024-06-18 10:55:29 -0700

    added user hashing password w bcrpyt

--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,16 @@
-
-const sequelize = require('../connection/config')
+const sequelize = require('../connection/config');
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 class User extends Model {}
 
 User.init({
-
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-
-
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
   name: {
     type: DataTypes.STRING,
     allowNull: false,
@@ -47,6 +44,20 @@ User.init({
   sequelize,
   modelName: 'User',
   timestamps: true,
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    },
+    beforeUpdate: async (user) => {
+      if (user.password) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    },
+  },
 });
 
 module.exports = User;
--- server/routes/api/UserRoutes.js
+++ server/routes/api/UserRoutes.js
@@ -2,32 +2,27 @@ const router = require('express').Router();
 const User = require('../../models/User');
 
 router.post('/', async (req, res) => {
-  console.log('Request Body:', req.body); // Logs  my request body for debugging
 
-  try {
+    // destrcutres req from body
     const { name, email, password, phoneNumber, address, vehicleDetails } = req.body;
+  
+    try {
+      const newUser = await User.create({
+        name,
+        email,
+        password,
+        phoneNumber,
+        address,
+        vehicleDetails,
+      });
 
-    // Validates  if required fields are not present in the request body
-    if (!name || !email || !password) {
-      return res.status(400).json({ error: 'Missing required fields' });
+      
+  
+      res.status(201).json({ message: 'User created successfully!', newUser });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
-
-    // Create new user
-    const newUser = await User.create({
-      name,
-      email,
-      password,
-      phoneNumber,
-      address,
-      vehicleDetails,
-    });
-
-    res.status(201).json({ message: 'User registered successfully!', newUser });
-  } catch (err) {
-    console.error('Error:', err); // Logs the error for debugging purposes
-    res.status(500).json({ error: err.message });
-  }
-});
+  });
 
 
 
