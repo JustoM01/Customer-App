413a258445885bfee326cfb1499b3f59ef381e40
unknown <eliseo101801@gmail.com>
2024-07-02 11:22:40 -0700

    updated react page and server route

--- server/routes/api/carEstimateRoutes.js
+++ server/routes/api/carEstimateRoutes.js
@@ -1,38 +1,21 @@
 const router = require('express').Router();
-const User = require('../../models/User');
 const carEstimate = require('../../models/carEstimate');
 const Price = require('../../models/Price');
 
 // Helper function to calculate estimated cost
 const calculateEstimatedCost = (basePrice, multiplier) => {
-  return basePrice * multiplier; // Perform the calculation based on your logic
+  return basePrice * multiplier; // Perform the calculation based on this logic
 };
 
 // Route to generate a quote
 router.post('/generate-quote', async (req, res) => {
-  const { userId, serviceTypeId } = req.body;
+  const { serviceTypeId, vehicleType } = req.body;
 
-
-
-//   first finds use by primarkey so id
-//   first finds use by primarkey so id
   try {
-    const user = await User.findByPk(userId);
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-
-
-    // saving vehicle details from user
-    
-    // saving vehicle details from user
-    // saving vehicle details from user
-    const vehicleDetails = user.vehicleDetails;
-
     const priceInfo = await Price.findOne({
       where: {
         serviceTypeId,
-        vehicleType: vehicleDetails.type,
+        vehicleType,
       },
     });
 
@@ -40,13 +23,10 @@ router.post('/generate-quote', async (req, res) => {
       return res.status(404).json({ error: 'Pricing information not found for the specified service type and vehicle type' });
     }
 
-
-
-    // passes basePrice from found in price table and mulitplier to helper function
+    // passes basePrice from found in price table and multiplier to helper function
     const estimatedCost = calculateEstimatedCost(priceInfo.basePrice, priceInfo.multiplier);
 
     const newEstimate = await carEstimate.create({
-      userId,
       serviceType: priceInfo.serviceTypeId,
       date: new Date(),
       estimatedCost,
